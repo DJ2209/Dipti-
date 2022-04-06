ede45d7d274ec4767a319adf8bc9ccbbc5cb40c5
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2022-04-07 02:07:30 +0530

    Developed Green Paradise

--- js/chart1.js
+++ js/chart1.js
@@ -1,26 +1,63 @@
-const labels = ["January", "February", "March", "April", "May", "June"];
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
 
 const data = {
   labels: labels,
   datasets: [
     {
-      label: "Sales in Month ",
+      label: "Sales of Money Plant ",
       backgroundColor: "rgb(255, 99, 132)",
       borderColor: "rgb(255, 99, 132)",
-      data: [0.1, 1.3, 2.5, 10, 12, 15, 20],
+      data: [1, 5, 15, 17, 23, 35, 47, 50],
     },
   ],
 };
 
 const config = {
-  type: 'bar',
+  type: "line",
   data: data,
   options: {
     scales: {
+      padding: 20,
       y: {
-        beginAtZero: true
-      }
-    }
+        beginAtZero: true,
+      },
+    },
+  },
+};
+const label = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+];
+
+const dats = {
+  labels: label,
+  datasets: [
+    {
+      label: "Sales of Zamia ZZ Black Plant",
+      backgroundColor: "#0ee049",
+      borderColor: "#0ee049",
+      data: [1, 7, 19, 23, 30, 33, 37, 60],
+    },
+  ],
+};
+
+const confi = {
+  type: "line",
+  data: dats,
+  options: {
+    scales: {
+      padding: 20,
+      y: {
+        beginAtZero: true,
+      },
+    },
   },
 };
 const myChart = new Chart(document.getElementById("myChart"), config);
+const myChar = new Chart(document.getElementById("myChar"), confi);
